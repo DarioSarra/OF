5ac39ca14b6b1313f2ce883d4a4b7bef051d5365
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-04-11 16:12:32 +0100

    cleanup

--- src/combine_sessions.jl
+++ src/combine_sessions.jl
@@ -5,7 +5,7 @@ function combine_sessions(DataIndex)
         r = select(DataIndex,(:bhv_file,:trk_file))[i]
         try
             provisory = combine_BhvTrk(r)
-            ongoing = append!(rows(ongoing), rows(provisory))
+            append!(rows(ongoing), rows(provisory))
         catch
             #@warn("Session not processed: DataIndex $i")
             println("Session not processed: DataIndex $i")
